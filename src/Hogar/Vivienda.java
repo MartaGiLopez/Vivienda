3cb91b6e59ec319deb9956520fcf198460c0ee6f
MartaGiLopez <4165690@alu.murciaeduca.es>
2024-02-25 19:56:04 +0100

    refactorización realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -10,22 +10,88 @@ package Hogar;
  * @author paqui
  */
 public class Vivienda {
+    // Atributos privados
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+   private double precio;
+  private  int numHabitaciones;
+   private double superficie;
+   private boolean parking;
+   private String estado;
+   private String propietario;
+   
+   //Constructor
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
-    
-    public void actualizarPrecio()
-    {
-        precio = precio - precio * 0.05;
+    //Constructor que recibe todos los parámetros
+    public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;
     }
+    //Setter y Getter para precio
     
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    //Setter y Getter para numHabitaciones
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    // Setter y Getter para superficie
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    // Setter y Getter para parking
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    // Setter y Getter para estado
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    // Setter y Getter para propietario
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
 }
+// Método actualizarPrecio con descuento
+    public void actualizarPrecio(double descuento) {
+        double porcentajeDescuento = descuento / 100.0;
+        double cantidadDescontada = this.precio * porcentajeDescuento;
+        this.precio -= cantidadDescontada;
+    }
+
+
