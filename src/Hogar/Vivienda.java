6e585cb731922c4a8c7ec20719a6245672c9b243
MartaGiLopez <4165690@alu.murciaeduca.es>
2024-02-25 20:17:05 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -3,29 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Hogar;
-
 /**
- *
- * @author paqui
+ * Representa una vivienda con detalles como precio, número de habitaciones, superficie,
+ * disponibilidad de parking, estado y propietario.
  */
 public class Vivienda {
-    // Atributos privados
-    
-   private double precio;
-  private  int numHabitaciones;
-   private double superficie;
-   private boolean parking;
-   private String estado;
-   private String propietario;
+    // Atributos de la clase
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
    
-   //Constructor
-    
-    public Vivienda (double precio)
-    {
+    /**
+     * Constructor para inicializar una vivienda solo con su precio.
+     * 
+     * @param precio El precio inicial de la vivienda.
+     */
+    public Vivienda(double precio) {
         this.precio = precio;
     }
-    //Constructor que recibe todos los parámetros
+    
+    /**
+     * Constructor para inicializar una vivienda con todos sus detalles.
+     * 
+     * @param precio El precio de la vivienda.
+     * @param numHabitaciones Número de habitaciones de la vivienda.
+     * @param superficie Superficie de la vivienda en metros cuadrados.
+     * @param parking Indica si la vivienda tiene parking (true) o no (false).
+     * @param estado El estado actual de la vivienda (nuevo, usado, etc.).
+     * @param propietario El nombre del propietario actual de la vivienda.
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -34,64 +43,122 @@ public class Vivienda {
         this.estado = estado;
         this.propietario = propietario;
     }
-    //Setter y Getter para precio
     
+    /**
+     * Establece el precio de la vivienda.
+     * 
+     * @param precio El nuevo precio de la vivienda.
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+
+    /**
+     * Devuelve el precio de la vivienda.
+     * 
+     * @return El precio de la vivienda.
+     */
     public double getPrecio() {
         return precio;
     }
-    //Setter y Getter para numHabitaciones
+
+    /**
+     * Establece el número de habitaciones de la vivienda.
+     * 
+     * @param numHabitaciones El nuevo número de habitaciones.
+     */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
+    /**
+     * Devuelve el número de habitaciones de la vivienda.
+     * 
+     * @return El número de habitaciones.
+     */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
-    // Setter y Getter para superficie
+    /**
+     * Establece la superficie de la vivienda.
+     * 
+     * @param superficie La nueva superficie de la vivienda en metros cuadrados.
+     */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
+    /**
+     * Devuelve la superficie de la vivienda.
+     * 
+     * @return La superficie de la vivienda en metros cuadrados.
+     */
     public double getSuperficie() {
         return superficie;
     }
 
-    // Setter y Getter para parking
+    /**
+     * Establece la disponibilidad de parking de la vivienda.
+     * 
+     * @param parking true si la vivienda tiene parking, false en caso contrario.
+     */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
+    /**
+     * Indica si la vivienda tiene parking.
+     * 
+     * @return true si la vivienda tiene parking, false en caso contrario.
+     */
     public boolean isParking() {
         return parking;
     }
 
-    // Setter y Getter para estado
+    /**
+     * Establece el estado de la vivienda.
+     * 
+     * @param estado El nuevo estado de la vivienda.
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * Devuelve el estado de la vivienda.
+     * 
+     * @return El estado de la vivienda.
+     */
     public String getEstado() {
         return estado;
     }
 
-    // Setter y Getter para propietario
+    /**
+     * Establece el propietario de la vivienda.
+     * 
+     * @param propietario El nuevo propietario de la vivienda.
+     */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
 
+    /**
+     * Devuelve el nombre del propietario de la vivienda.
+     * 
+     * @return El nombre del propietario.
+     */
     public String getPropietario() {
         return propietario;
     }
-}
-// Método actualizarPrecio con descuento
+
+    /**
+     * Actualiza el precio de la vivienda aplicando un descuento.
+     * 
+     * @param descuento El porcentaje de descuento a aplicar sobre el precio actual.
+     */
     public void actualizarPrecio(double descuento) {
         double porcentajeDescuento = descuento / 100.0;
-        double cantidadDescontada = this.precio * porcentajeDescuento;
-        this.precio -= cantidadDescontada;
-    }
-
-
+        double cantidadDescontada; 
+ }
+}
